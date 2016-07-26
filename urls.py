0fbaf3c458017146b53f54b40b1b0c1bdd933ba5
vigneshpr <vigneshwarann@techaffinity.com>
2016-07-26 13:42:46 +0530

    first commit

--- urls.py
+++ urls.py
@@ -1,10 +1,2 @@
-from django.conf.urls import url
+samle edit
 
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index_test, name='index'),
-    url(r'^test1/', views.index_test1, name='index'),
-    url(r'^test2/', views.index_test2, name='index'),
-    url(r'^test3/', views.index_test3, name='index'),
-]
